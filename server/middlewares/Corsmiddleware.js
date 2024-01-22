25b3536adbc017ef3f971f8eeba5a6ebc8fdb129
Haseeeb1 <haseebhassan701@gmail.com>
2024-01-22 22:07:29 +0500

    first

--- server/middlewares/Corsmiddleware.js
+++ server/middlewares/Corsmiddleware.js
@@ -1,6 +1,9 @@
 const Corsmiddleware = (req, res, next) => {
   // CORS headers
-  res.header("Access-Control-Allow-Origin", "YOUR_URL"); // restrict it to the required domain
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://workease-bese27c.vercel.app"
+  ); // restrict it to the required domain
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   // Set custom headers for CORS
   res.header(
